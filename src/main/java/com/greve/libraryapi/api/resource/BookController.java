205ad6a7892c6c174c167dca68383d4ce3cc1834
henriquegreve <xb9gx5zs1>
2022-01-17 16:04:36 -0300

    Alteracoes para testes

--- src/main/java/com/greve/libraryapi/api/resource/BookController.java
+++ src/main/java/com/greve/libraryapi/api/resource/BookController.java
@@ -64,7 +64,7 @@ public class BookController {
 
     @PutMapping("{id}")
     @ApiOperation("Update a book")
-    public BookDTO update(@PathVariable Long id, BookDTO dto) {
+    public BookDTO update(@PathVariable Long id, @RequestBody @Valid BookDTO dto) {
         return service.getById(id).map(book -> {
             book.setAuthor(dto.getAuthor());
             book.setTitle(dto.getTitle());
